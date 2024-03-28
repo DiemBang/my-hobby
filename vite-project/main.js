76831a077916cc89630544316250ea8956fadc99
Diem Bang <diembang09@gmail.com>
2024-03-29 00:14:02 +0100

    :sparkles: add interactive map to travel page - CHERRY-PICK THIS

--- vite-project/main.js
+++ vite-project/main.js
@@ -11,20 +11,19 @@ function addNextAndPreviousEventListeners() {
   let nextButtons = document.getElementsByClassName("next");
   let prevButtons = document.getElementsByClassName("prev");
 
-  Array.from(nextButtons).forEach(element => {
+  Array.from(nextButtons).forEach((element) => {
     element.addEventListener("click", function (event) {
       event.preventDefault();
       plusSlides(1);
     });
   });
-  
-  Array.from(prevButtons).forEach(element => {
+
+  Array.from(prevButtons).forEach((element) => {
     element.addEventListener("click", function (event) {
       event.preventDefault();
       plusSlides(-1);
     });
   });
-  
 }
 
 let aboutLink = document.getElementById("aboutLink");
@@ -32,7 +31,6 @@ let travelLink = document.getElementById("travelLink");
 let galleryLink = document.getElementById("galleryLink");
 let aboutPage = document.getElementById("aboutPage");
 let galleryPage = document.getElementById("galleryPage");
-
 
 aboutLink.addEventListener("click", function (event) {
   event.preventDefault();
@@ -170,11 +168,11 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -182,15 +180,19 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
