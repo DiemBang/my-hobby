21faa4d5b5282ac833ab88982e07e67f8d1edabd
Diem Bang <diembang09@gmail.com>
2024-03-28 15:55:52 +0100

    :sparkles: add top nav links

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,8 +1,30 @@
 function responsiveMenu() {
   var topNav = document.getElementById("myTopnav");
   if (topNav.className === "topnav") {
-      topNav.className += " open";
+    topNav.className += " open";
   } else {
-      topNav.className = "topnav";
+    topNav.className = "topnav";
   }
 }
+
+let aboutLink = document.getElementById("aboutLink");
+let travelLink = document.getElementById("travelLink");
+let galleryLink = document.getElementById("galleryLink");
+let aboutPage = document.getElementById("aboutPage");
+
+aboutLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  aboutPage.classList.remove("hidden");
+})
+
+travelLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  aboutPage.classList.add("hidden");
+})
+
+galleryLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  aboutPage.classList.add("hidden");
+});
+
+
